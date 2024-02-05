0c6e1511c2584612e01908d1e017fc07bfb70420
Ross Tauchert <57552771+rdtauche@users.noreply.github.com>
2024-02-04 19:57:38 -0500

    working solution but needs cleanup

--- Assets/script.js
+++ Assets/script.js
@@ -1,39 +1,44 @@
 // CU Bootcamp: Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//Ross: Step #1 - this function generates the password and lists all characters that are allowed in creating it
+//Ross: Function #1 - this function generates the password and lists all characters that are allowed in creating it
 function getPassword() {
-  var number = 0123456789;
-  var special = "!@#$%^&*()_+-=<>/?[]{}\|";
-  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var lowercase = "abcdefghijklmnopqrstuvwxyz";
-  var length = prompt("Your password must contain between 8 and 128 characters in length, please input the length you want");
-}
-
-// Ross: Step #2 - if the input provided is less than 8 or greater than 128, we return an error message
-if (length < 8 || length > 128 || isNaN(length)) {
-  alert("Your password does not meet the minimum criteria.  Passwords must be between 8 and 128 characters in length, please try again");
-} 
-
-else {
-  var numbers = confirm("Your password must have a number, please click OK to continue");
-  var specials = confirm("Your password must have special characters, please click OK to continue");
-  var uppercases = confirm("Your password must include uppercase letters, please click OK to continue");
-  var lowercases = confirm("Your password must contain lowercase letters, please click OK to continue");
-}
-
-for (i = 0; i < length; i++) {
-  randomKey += multiSelect[Math.floor(Math.random() * multiSelect.length)]
-}
-
-// Write password to the #password input. The text box area where the newly generated password will appear.
-var passwordText = document.querySelector("#password");
-function writePassword() {
-if (password.length === 0) {
+  var numeric = "0123456789";
+  var symbols = "!@#$%^&*()_+-=<>/?[]{}\|";
+  var uppercaseABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowercaseABC = "abcdefghijklmnopqrstuvwxyz";
+  var multiSelect = "";
+  var randomKey = "";
+  var keyLength = prompt("Your password must contain between 8 and 128 characters in length, please input the length you want");
+  if (keyLength < 8 || keyLength > 128) {
+    alert("Your password does not meet the minimum criteria.  Passwords must be between 8 and 128 characters in length, please try again");
     return;
-}
-passwordText.value = randomKey;
+  } else {
+    var numericOptions = confirm("Your password must have a number, please click OK to continue");
+    var specialSymbols = confirm("Your password must have special characters, please click OK to continue");
+    var upperAlpha = confirm("Your password must include uppercase letters, please click OK to continue");
+    var lowerAlpha = confirm("Your password must contain lowercase letters, please click OK to continue");
+  }
+  if (upperAlpha) {multiSelect += uppercaseABC;}
+  if (lowerAlpha) {multiSelect += lowercaseABC;}
+  if (numericOptions) {multiSelect += numeric;}
+  if (specialSymbols) {multiSelect += symbols;}
+  for (i = 0; i < length; i++) {
+    randomKey += multiSelect[Math.floor(Math.random() * multiSelect.length)]
+  }
+  return(randomKey);
 }
 
-// Add event listener to generate button. This is a Call To Action (CTA button)
-generateBtn.addEventListener("click", function () { writePassword(getPassword()) });
+// Write password to the #password input
+var passwordText = document.querySelector("#password");
+
+//Ross:  Function #2: write password function and call it
+function writePassword(randomKey) {
+if (password.length === 0) {
+  return;
+}
+  passwordText.value = randomKey;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", function() {writePassword(getPassword()) });
