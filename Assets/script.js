488d771f1c6e141d4d4b3a2779287f13003506ed
Ross Tauchert <57552771+rdtauche@users.noreply.github.com>
2024-02-07 06:25:04 -0500

    added detail to script.js

--- Assets/script.js
+++ Assets/script.js
@@ -9,15 +9,15 @@ function getPassword() {
   var lowercaseABC = "abcdefghijklmnopqrstuvwxyz";
   var multiSelect = "";
   var randomKey = "";
-  var keyLength = prompt("Your password must contain between 8 and 128 characters in length, please input the length you want");
+  var keyLength = window.prompt("Your password must contain between 8 and 128 characters in length, please input the length you want");
   if (keyLength < 8 || keyLength > 128) {
-    alert("Your password does not meet the minimum criteria.  Passwords must be between 8 and 128 characters in length, please try again");
+    window.alert("Your password does not meet the minimum criteria.  Passwords must be between 8 and 128 characters in length, please try again");
     return;
   } else {
-    var numericOptions = confirm("Your password must have a number, please click OK to continue");
-    var specialSymbols = confirm("Your password must have special characters, please click OK to continue");
-    var upperAlpha = confirm("Your password must include uppercase letters, please click OK to continue");
-    var lowerAlpha = confirm("Your password must contain lowercase letters, please click OK to continue");
+    var numericOptions = window.confirm("Your password must have a number, please click OK to continue");
+    var specialSymbols = window.confirm("Your password must have special characters, please click OK to continue");
+    var upperAlpha = window.confirm("Your password must include uppercase letters, please click OK to continue");
+    var lowerAlpha = window.confirm("Your password must contain lowercase letters, please click OK to continue");
   }
   if (upperAlpha) {multiSelect += uppercaseABC;}
   if (lowerAlpha) {multiSelect += lowercaseABC;}
